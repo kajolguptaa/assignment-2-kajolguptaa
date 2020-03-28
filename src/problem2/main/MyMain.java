9d2a0ab9d9c1404c355805551de4338f1e5495f6
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2020-03-28 16:10:33 +0530

    init commit add main class add node class add binarySearchTree class

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,5 +7,29 @@
 package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem2.main.binarySearchTree.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+// executable class
+// use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyQueue preQueue = new MyQueue();
+        MyQueue postQueue = new MyQueue();
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree(preQueue, postQueue);
+        myBinarySearchTree.insert(15);
+        myBinarySearchTree.insert(10);
+        myBinarySearchTree.insert(50);
+        myBinarySearchTree.insert(55);
+        myBinarySearchTree.insert(60);
+        myBinarySearchTree.postOrder(myBinarySearchTree.getRoot());
+        myBinarySearchTree.preOrder(myBinarySearchTree.getRoot());
+        System.out.println("PreOrder:");
+        preQueue.show();
+        System.out.println("PostOrder:");
+        postQueue.show();
+        System.out.println();
+    }
+
 }
